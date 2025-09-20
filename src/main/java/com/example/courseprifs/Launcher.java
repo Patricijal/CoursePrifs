694cd15055af247c2025e2485353d977caf4ec7d
Patricija Legataitė 20232345 <patricija.legataite@stud.vilniustech.lt>
2025-09-20 15:52:38 +0300

    Initial commit

---
+++ src/main/java/com/example/courseprifs/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.courseprifs;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.courseprifs {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires lombok;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    //requires static lombok;
+    //requires com.example.courseprifs;
+
+    opens com.example.courseprifs to javafx.fxml;
+    exports com.example.courseprifs;
+}
